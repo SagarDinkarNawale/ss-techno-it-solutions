ba6eb8eadab2a07673fd58aa630da7d8d50c84dd
Sagar Nawale <nawalesagar0@gmail.com>
2024-09-22 23:45:32 +0530

    all unnecessary changes removed and make ready to production

--- src/main/java/com/org/ss/techno/controller/MainController.java
+++ src/main/java/com/org/ss/techno/controller/MainController.java
@@ -11,36 +11,6 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/soh")
-    public String soh() {
-        return "soh";
-    }
-
-    @GetMapping("/pmo-as-a-service")
-    public String pmoService() {
-        return "pmo-as-a-service";
-    }
-
-    @GetMapping("/managed-teams")
-    public String managedTeams() {
-        return "managed-teams";
-    }
-
-    @GetMapping("/insights")
-    public String insights() {
-        return "insights";
-    }
-
-    @GetMapping("/global-capability-center")
-    public String globalCapabilityCenter() {
-        return "global-capability-center";
-    }
-
-    @GetMapping("/digital-transformation")
-    public String digitalTransformation() {
-        return "digital-transformation";
-    }
-
     @GetMapping("/contact")
     public String contact() {
         return "contact";
@@ -55,23 +25,8 @@ public class MainController {
         return "services";
     }
 
-    @GetMapping("/bot")
-    public String bot() {
-        return "bot";
-    }
-
     @GetMapping("/about-us")
     public String aboutUs() {
         return "about-us";
-    }
-
-    @GetMapping("/index_test")
-    public String home_test() {
-        return "index_test";
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return "test";
     }
 }
